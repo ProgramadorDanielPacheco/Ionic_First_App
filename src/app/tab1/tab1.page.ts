fc6284e51b7800d4880754275412756a363150c2
espeon <danielpacheco842005@gmail.com>
2023-02-13 11:42:12 -0500

    Primera app Ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['style.css']
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Recordatorio',
+      message: 'Recuerda Comprar Pan',
+      buttons: ['OK', 'CANCEL', 'ERROR']
+    });
+
+    await alert.present()
+  }
 
 }
